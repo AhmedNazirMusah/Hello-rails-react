e034740cb77b6955a5be989ce798920e38a62f23
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-02-28 13:16:42 +0000

    Setup with react and edux

---
+++ app/javascript/application.js
@@ -0,0 +1,17 @@
+// Entry point for the build script in your package.json
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/configureStore';
+
+
+ReactDOM.render(
+  <Provider store={store}>  
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
